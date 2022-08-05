2f39a268eaa4e727966eca665998a65ca524a72a
Akaash <109869253+akaash1901006@users.noreply.github.com>
2022-08-05 12:36:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,15 +8,15 @@ document.querySelector("#nav-close").onclick = () => {
   navbar.classList.remove("active");
 };
 
-let searchForm = document.querySelector(".search-form");
+// let searchForm = document.querySelector(".search-form");
 
-document.querySelector("#search-btn").onclick = () => {
-  searchForm.classList.add("active");
-};
+// document.querySelector("#search-btn").onclick = () => {
+//   searchForm.classList.add("active");
+// };
 
-document.querySelector("#close-search").onclick = () => {
-  searchForm.classList.remove("active");
-};
+// document.querySelector("#close-search").onclick = () => {
+//   searchForm.classList.remove("active");
+// };
 
 window.onscroll = () => {
   navbar.classList.remove("active");
